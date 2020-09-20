534d0b212782dfddb4860cc5f08fd8947a693a70
Luélly Cabral <luellycabral21@gmail.com>
2020-09-20 20:48:34 -0300

    Adiciona o nome completo

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace NomeCompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+        //<--- inicio ( Nome e Sobrenome)--->//
+        Console.Write("Digite Seu Nome:");
+        string nome = Console.ReadLine();
+        Console.Write("Digite seu Sobrenome:");
+        string sobrenome = Console.ReadLine();
+        Console.Write("\n");
+        //<--- fim (Nome e Sobrenome )--->//
+
+        //<--- inicio (nome sobrnome)--->//
+        Console.Write("nome completo: {nome} {sobrenome}");
+        Console.Write("\n");
+        //<---fim (nome)--->//
+
+        //<---inicio (nome catálago)--->//
+        Console.WriteLine("nome catálago {sobrenome . ToUpper ()} , {nome}");
+        Console.Write("\n");
+        //<---fim (nome catálgo)--->//
+
+        //<---(para sair)--->//
+        Console.WriteLine("clik a técla ENTER para sair");
+        Console.ReadKey();
+        //<---fim --->//
+}
+  
+    } 
+
+} 
+        
+
+        
+
+
+
